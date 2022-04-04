5f39d234a3cc30a9f916bdfb6304aa14be290111
Noah Albers <albers-noah@pm.me>
2022-04-04 07:52:52 +0200

    Implemented further required configurations inside the Environment

--- src/Environment.ts
+++ src/Environment.ts
@@ -1,9 +1,15 @@
 export class Environment{
 
     public readonly withComments : boolean;
+    public readonly ledAmount : number;
+    public readonly preprocessingCode: string;
+    public readonly ledPin : number;
 
-    public constructor(withComments : boolean){
+    public constructor(ledAmount: number,withComments : boolean, preprocessingCode: string, ledPin: number){
         this.withComments = withComments;
+        this.ledAmount = ledAmount;
+        this.preprocessingCode = preprocessingCode;
+        this.ledPin = ledPin;
     }
 
 }
