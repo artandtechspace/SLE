73d05324ca6d15d6ce8014239c3a16dea63e4f5a
Noah Albers <albers-noah@pm.me>
2022-04-04 07:53:45 +0200

    Removed some currently unwanted complexity from the modules.

--- src/modules/ModuleBase.ts
+++ src/modules/ModuleBase.ts
@@ -1,38 +1,13 @@
+import { Config } from "../Config";
 import { Environment } from "../Environment";
 import { VariableSystem } from "../variablesystem/VariableSystem";
-
-// Constants that expose the types of the generators.
-// Start-only can only run on start and will result in an error if called after any normal generators.
-// and generate normal can use the setup, but will use the loop
-export const GENERATOR_START_ONLY : Number = 1;
-export const GENERATOR_NORMAL : Number = 0;
+import { ModuleReturn } from "./ModuleReturn";
 
 /**
  * The module-base is the element that takes in a configuration file and generate the code based on the provided information and environment.
  */
 
 export class ModuleBase{
-
-    /**
-     * Returns when this code can run. If normale is used, the code can use the setup-function but will use the loop function to run continuesly.
-     * But selecting a start_only generator to run after a start_normal generator, will result in an error. 
-     */
-    public getType() : Number{
-        return GENERATOR_NORMAL;
-    }
-
-    /**
-     * Returns an object that contains all configurations that can be passed to the generator.
-     * This is in the following format:
-     * var-name: {
-     *  type: (String) type of the variable or string-array if multiple types are allowed
-     *  (Optional) validator: (function) that takes in a value as the parameter and returns if the given value is valid (true/false).
-     * }
-     */
-    public getConfigSkeleton() : Object{
-        return {}
-    }
-
     /**
      * This function is here to generate the code for the Arduino/Mc.
      * 
@@ -44,7 +19,7 @@ export class ModuleBase{
      * }
      * 
      */
-    public generateCode(env : Environment, varSys : VariableSystem) : Object{
+    public generateCode(env : Environment, varSys : VariableSystem, config: Config) : ModuleReturn{
         return {}
     }
 }
