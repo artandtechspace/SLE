2077b063974423a7aeb2f2eee67443cdf4728f48
Noah Albers <albers-noah@pm.me>
2022-09-18 14:40:22 +0200

    - Testimplementation for the windows-export - Added installing animation - Added icon as .ico

--- src/electron/index.js
+++ src/electron/index.js
@@ -1,16 +1,16 @@
 const { app, BrowserWindow } = require('electron');
 const path = require("path");
 
+// Prevents the app from launching during installation
+if(require("electron-squirrel-startup")) return app.quit();
+
 app.on('ready', () => {
 
   // Creates the browser-window
   const win = new BrowserWindow({
     width: 600*2,
     height: 400*2,
-    icon: __dirname+"/../resources/icon/icon.png",
-    webPreferences:{
-      
-    }
+    icon: path.resolve(__dirname, "/../resources/icon/icon.png"),
   });
 
   const indexHTML = path.join(__dirname + '/../index.html');
