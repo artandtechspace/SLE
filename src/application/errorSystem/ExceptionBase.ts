6c0207fd8a03060652b41948063dbb199cd5c7b0
Noah Albers <albers-noah@pm.me>
2022-11-12 21:42:21 +0100

    Added Release-Beta-0.0.1

--- src/application/errorSystem/ExceptionBase.ts
+++
@@ -1,8 +0,0 @@
-export abstract class ExceptionBase{
-    // Exception-message
-    public readonly message: string;
-
-    protected constructor(message: string){
-        this.message = message;
-    }
-}
--- src/application/utils/EquationUtils.ts
+++
@@ -1,6 +0,0 @@
-import { Variable } from "../codegenerator/variablesystem/Variable";
-
-// Takes in an equation and replaces the variables
-export function printEquation(equation: string, vars: {[key: string]: Variable|number}){
-    return equation.replace(/\$[a-zA-Z]+/gi, match=>vars[match.substring(1)].toString());
-}
